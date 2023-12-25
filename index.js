2def7ca0b25516c9a81ac08f01801b35ae1f008c
Om Pandey <ompandey007j@gmail.com>
2023-12-25 18:38:05 +0530

    pushig the previous guesses

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-const randomNumber=parseInt(Math.random()*100+1)
+const randomNumber = parseInt(Math.random() * 100 + 1);
 
-const pre=[]
+const pre = [];
 
-const home=document.querySelector('form')
-const guess=document.querySelector('#guessField').value
-const submit=document.querySelector('#subt')
-const previous=document.querySelector('.guesses')
+const home = document.querySelector("form");
+const guess = document.querySelector("#guessField").value;
+const submit = document.querySelector("#subt");
+const previous = document.querySelector(".guesses");
 
-const remaining=document.querySelector('.lastResult')
-const result=document.querySelector('#results')
+const remaining = document.querySelector(".lastResult");
+const result = document.querySelector("#results");
 
-home.addEventListener('submit',(e)=>{
-e.preventDefault()
-valid(guess);
+home.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(guess)
+    valid(guess);
+});
+ 
 
-})
-
-function valid(guess){
-    if(isNaN(guess) || guess < 1 || guess > 100){
-        result.innerHTML = "Invalid Input";
-    }else{
-        check(guess);
-    } 
+function valid(guess) {
+  if (isNaN(guess) || guess < 1 || guess > 100) {
+  } else {
+    result.innerHTML = "Invalid Input";
+    check(guess);
+  }
 }
 
-function check(guess){
-if(guess<randomNumber){
+function check(guess) {
+  if (guess < randomNumber) {
     result.innerHTML = "Number is too low";
-}else if(guess<randomNumber){
+  } else if (guess < randomNumber) {
     result.innerHTML = "Number is too high";
-}else if(guess==randomNumber){
+  } else if (guess == randomNumber) {
     result.innerHTML = "Congractulation you guesed the right number";
-
+  }
+  pre.push(guess)
+//   guess=
 }
-
-}
-
-
