57b751554f2f87a578ca0f755ebef4214c5de49f
Om Pandey <ompandey007j@gmail.com>
2023-12-26 19:01:46 +0530

    done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const randomNumber = parseInt(Math.random() * 100 + 1);
+console.log(randomNumber);
 
 const pre = [];
 
@@ -6,14 +7,13 @@ const home = document.querySelector("form");
 console.log(guess);
 const submit = document.querySelector("#subt");
 const previous = document.querySelector(".guesses");
-
 const remaining = document.querySelector(".lastResult");
 const result = document.querySelector("#results");
 
 home.addEventListener("submit", (e) => {
   e.preventDefault();
   const guess = document.querySelector("#guessField").value;
-  
+  document.querySelector("#guessField").focus();
   console.log(guess);
   valid(guess);
 });
@@ -21,18 +21,38 @@ home.addEventListener("submit", (e) => {
 function valid(guess) {
   if (isNaN(guess) || guess < 1 || guess > 100) {
     result.innerHTML = "Invalid Input";
+    document.querySelector("#guessField").value = "";
   } else {
     check(guess);
   }
 }
 
 function check(guess) {
-  if (guess < randomNumber) {
-    result.innerHTML = "Number is too low";
-  } else if (guess < randomNumber) {
-    result.innerHTML = "Number is too high";
-  } else if (guess == randomNumber) {
-    result.innerHTML = "Congractulation you guesed the right number";
+  if (remaining.textContent > 1) {
+    if (guess < randomNumber) {
+      pre.push(guess);
+      result.innerHTML = "Number is too low";
+      result.style.color = "white";
+
+      document.querySelector("#guessField").value = "";
+      remaining.textContent = remaining.textContent - 1;
+    } else if (guess > randomNumber) {
+      pre.push(guess);
+      result.innerHTML = "Number is too high";
+      result.style.color = "white";
+      document.querySelector("#guessField").value = "";
+      remaining.textContent = remaining.textContent - 1;
+    } else if (guess == randomNumber) {
+      result.style.color = "#1fd655";
+      result.innerHTML = "Congratualtion! you won";
+      submit.disabled = true;
+    }
+  } else {
+    result.style.color = " 	#EE4B2B";
+    result.innerHTML = `Get Better...reload to retry`;
+    remaining.textContent = 0;
+    submit.disabled = true;
   }
-  pre.push(guess);
+
+  previous.innerHTML = `${pre}`;
 }
